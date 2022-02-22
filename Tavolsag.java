708fcca3ed755bc8be23d790dc406203e1797c30
Székely Márton <szekelymarton001@gmail.com>
2022-02-22 08:33:29 +0100

    Kezdés

---
+++ Tavolsag.java
@@ -0,0 +1,19 @@
+/*
+* File: Tavolsag.java
+* Author: Székely Márton
+* Group: Szoft I/N
+* Date: 2021-02-22
+* Github: https://github.com/Lijakudo/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class Tavolsag {
+	public static void main(String[] args) {
+		
+		System.out.println("Székely Márton SZOFT I/N");
+		System.out.println("2022-02-22");
+		
+		}
+	}
