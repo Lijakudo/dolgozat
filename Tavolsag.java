8ce1306c3bba3c07946531e6d82bd9ec87775920
Székely Márton <szekelymarton001@gmail.com>
2022-02-22 08:37:59 +0100

    Bekérés kész

--- Tavolsag.java
+++ Tavolsag.java
@@ -12,8 +12,14 @@ import java.util.Scanner;
 class Tavolsag {
 	public static void main(String[] args) {
 		
-		System.out.println("Székely Márton SZOFT I/N");
+		System.out.println("Szekely Marton SZOFT I/N");
 		System.out.println("2022-02-22");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Adja meg a magassagot: ");
+		double mag = scan.nextDouble();
+		System.out.print("Adja meg a szoget: ");
+		double szog = scan.nextDouble();
 		
 		}
 	}
